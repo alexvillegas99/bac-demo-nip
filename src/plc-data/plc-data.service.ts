2f17b87a6defc5cfdf9a60a334ad61214d98dd5a
Alex Villegas <av08554@gmail.com>
2025-04-09 19:29:19 -0500

    1

--- src/plc-data/plc-data.service.ts
+++ src/plc-data/plc-data.service.ts
@@ -13,35 +13,6 @@ export class PlcDataService {
     @InjectModel('plc')
     private readonly plcDataBase: Model<plcData>,
   ) {
-    //actualizar la data cada 5 segundos en donde la IP sea 172.16.8.9
-
-    setInterval(async () => {
-      // Calcular de forma aleatoria corriente, frecuencia, potencia, voltaje, processValue
-      const corriente = Math.floor(Math.random() * 100) + 1;
-      const voltaje = Math.floor(Math.random() * 220) + 1;
-      const frecuencia = Math.floor(Math.random() * 60) + 1;
-      const potencia = Math.floor(Math.random() * 1000) + 1;
-      const processValue = Math.floor(Math.random() * 100) + 1;
-
-      // Suponiendo que siempre actualizas un documento específico por su _id o algún identificador único
-      await this.plcDataBase.findOneAndUpdate(
-        { IP: '172.16.8.9' }, // filtro
-        {
-          $set: {
-            Data: {
-              corriente,
-              voltaje,
-              frecuencia,
-              potencia,
-              processValue,
-              timestamp: new Date(),
-            },
-          },
-        },
-        { upsert: true, new: true },
-      );
-      console.log('Data updated successfully!');
-    }, 5000);
   }
 
   async find(body: any) {
